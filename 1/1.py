04050efe56739c8bf1d021b551a39967a033e54e
Baimyrza <baimurza.lesbek@gmail.com>
2022-03-13 16:11:42 +0600

    deleted

--- 1/1.py
+++
@@ -1,9 +0,0 @@
-class MyClass:
-    def __init__(self):
-        self.text = ""
-    
-    def getString(self):
-        self.text = input("Enter text: ")
-
-    def printString(self):
-        print(self.text.upper())
--- 1/6.py
+++
@@ -1,3 +0,0 @@
-a = list(map(int, input().split())) 
-is_prime = list(filter(lambda i: all(i%j!=0 for j in range(2, i//2)), a))
-print(is_prime)
